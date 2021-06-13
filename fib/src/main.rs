493c99ca05c3bd2113cb1d8856b5a63d78c2d7c1
Steven Nguyen <steven7c8@gmail.com>
2021-06-12 18:47:35 -0700

    fib: change to accept args as file

--- fib/src/main.rs
+++ fib/src/main.rs
@@ -1,11 +1,19 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let n = args[1].parse::<u64>().unwrap();
-    let k = args[2].parse::<u64>().unwrap();
-    let mut pop_juvenile = 1;
-    let mut pop_mature = 0;
+    let filename = &args[1];
+    let filecontent = fs::read_to_string(filename)
+        .expect("Could not read file.");
+
+    let mut inputs = filecontent.lines().next().unwrap().split(" ");
+
+    let n = inputs.next().unwrap().to_string().parse::<u64>().unwrap();
+    let k = inputs.next().unwrap().to_string().parse::<u64>().unwrap();
+
+    let mut pop_juvenile: u64 = 1;
+    let mut pop_mature: u64 = 0;
     let mut pop_born: u64;
 
     for _ in 1..n {
